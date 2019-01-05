755eaa08c9ddc3e93d8beefc1e0b640586ca82ea
sam1610 <samir.amri@gmail.com>
2019-01-05 15:57:26 +0300

    routing  Welcome Page

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -14,10 +14,15 @@ import { PageNotFoundComponent } from './page-not-found.component';
 import { ProductModule } from './products/product.module';
 import { UserModule } from './user/user.module';
 import { MessageModule } from './messages/message.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
-    BrowserModule,
+    BrowserModule,RouterModule.forRoot([
+      {path:"welcome", component:WelcomeComponent},
+      {path:'',redirectTo:"welcome", pathMatch:"full"},
+      { path:"**", component:PageNotFoundComponent}
+    ]),
     HttpClientModule,
     InMemoryWebApiModule.forRoot(ProductData, { delay: 1000 }),
     ProductModule,
